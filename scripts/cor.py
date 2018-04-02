f60cdc8cb4d9437c8eb5c847fdc50d6e74214b47
decoejz <decoejz@gmail.com>
2018-04-02 15:30:34 -0300

    atualizacoes...

--- scripts/cor.py
+++ scripts/cor.py
@@ -14,22 +14,108 @@ from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-import findobjs as procura_obj
+import findobjs as po
+import numpy as np
+# import le_scan as sc
 
 bridge = CvBridge()
 
 cv_image = None
-media = []
-centro = []
+objeto_1 = False
+objeto_2 = False
+perigo = False
+
 atraso = 1.5
 
-check_delay = False # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados
+check_delay = True # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados
+	
+def contorno_quadrado():
+	distancia_risco = 5#Checar qual a unidade(m, cm, mm....)
+				
+	#Testando se o robo nao esta em perigo.
+	perigo = sc.laser_scan(distancia_risco)
+	if perigo:
+		vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
+		return (vel)
+	
+	else:
+		tempo_lado = 1#Ver quanto o robo precisa dormir para desenhar um lado
+		velocidade_desenho = -0.5#Velocidade que ele andará em um lado do quadrado.
+
+		vel = Twist(Vector3(velocidade_desenho,0,0), Vector3(0,0,0))#Velocidade para andar para frente
+		velocidade_saida.publish(vel)#Aplicando a velocidade no robo
+		rospy.sleep(tempo_lado)#Colocar o robo para dormir, para assim poder andar o espaço desejado.######VERIFICAR SE É ESSE O CÓDIGO DE DORMIR###
+		
+		###############################################################
+		####Ver a necessidade de zerar a velocidade nesse ponto!!!!####
+		###############################################################
+
+		perigo = sc.laser_scan(distancia_risco)
+		if perigo:
+			vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
+			return (vel)
+
+		else:
+			tempo_rotacao = 1#Checar quanto tempo precisa para ele virar apenas 90 graus
+
+			vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))#Girando o robo em 90 graus. Ver qual a velocidade ideal para isso.
+			velocidade_saida.publish(vel)
+			rospy.sleep(tempo_rotacao)#Colocar o robo para dormir para poder girar os 90 graus.
+
+			vel = Twist(Vector3(velocidade_desenho,0,0), Vector3(0,0,0))
+			velocidade_saida.publish(vel)
+			rospy.sleep(tempo_lado)
+
+			###############################################################
+			####Ver a necessidade de zerar a velocidade nesse ponto!!!!####
+			###############################################################
+
+			perigo = sc.laser_scan(distancia_risco)
+			if perigo:
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
+				return (vel)
+
+			else:
+				tempo_rotacao = 1#Checar quanto tempo precisa para ele virar apenas 90 graus
+
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))#Girando o robo em 90 graus. Ver qual a velocidade ideal para isso.
+				velocidade_saida.publish(vel)
+				rospy.sleep(tempo_rotacao)#Colocar o robo para dormir para poder girar os 90 graus.
+
+				vel = Twist(Vector3(velocidade_desenho,0,0), Vector3(0,0,0))
+				velocidade_saida.publish(vel)
+				rospy.sleep(tempo_lado)
+
+				###############################################################
+				####Ver a necessidade de zerar a velocidade nesse ponto!!!!####
+				###############################################################
+
+				perigo = sc.laser_scan(distancia_risco)
+				if perigo:
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
+					return (vel)
+
+				else:
+					tempo_rotacao = 1#Checar quanto tempo precisa para ele virar apenas 90 graus
+
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))#Girando o robo em 90 graus. Ver qual a velocidade ideal para isso.
+					velocidade_saida.publish(vel)
+					rospy.sleep(tempo_rotacao)#Colocar o robo para dormir para poder girar os 90 graus.
+
+					vel = Twist(Vector3(velocidade_desenho,0,0), Vector3(0,0,0))
+					velocidade_saida.publish(vel)
+					rospy.sleep(tempo_lado)
+
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+					return (vel)
+
 
 def roda_todo_frame(imagem):
 	print("frame")
 	global cv_image
-	global media
-	global centro
+	global objeto_1
+	global objeto_2
+	global perigo
 
 	now = rospy.get_rostime()
 	imgtime = imagem.header.stamp
@@ -40,22 +126,19 @@ def roda_todo_frame(imagem):
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-		media, centro = identifica_cor(cv_image)
+		#perigo = scaner()
+		objeto_1 = po.findobj1(cv_image)
+		#objeto_2 = po.findobj1(imagem)
 		depois = time.clock()
 		cv2.imshow("Camera", cv_image)
 	except CvBridgeError as e:
 		print('ex', e)
-	
-
 
 if __name__=="__main__":
 
 	rospy.init_node("cor")
 	# Para usar a Raspberry Pi
 	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
-	
-	# Para usar a webcam 
-	#recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
@@ -65,49 +148,22 @@ if __name__=="__main__":
 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
 
 			recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
-			achou_obj_1 = procura_obj.findobj1(recebedor)
-			
-			if achou_obj_1:
+
+			if perigo:
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+			elif objeto_1:
 				vel = Twist(Vector3(-0.5,0,0), Vector3(0,0,0))
-# 			if len(media) != 0 and len(centro) != 0:
-# 				dif_x = media[0]-centro[0]
-# 				dif_y = media[1]-centro[1]
-# 				if math.fabs(dif_x)<30: # Se a media estiver muito proxima do centro anda para frente
-# 			# 		vel = Twist(Vector3(-0.5,0,0), Vector3(0,0,0))
-# 			# 	else:
-# 			# 		if dif_x > 0: # Vira a direita
-# 			# 			vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
-# 			# 		else: # Vira a esquerda
-# 			# 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0.2))
-# 			# velocidade_saida.publish(vel)
-# 			# rospy.sleep(0.01)
-# ######################################################################################################
-# 					#print("dif_x",dif_x)##########Saber qual a grandeza desse valor###########################
-# ######################################################################################################
-# 					#Fazer uma condição if, elif, else para diferentes distancias.
-# 					#Quanto mais perto, menos velocidade
-# 					#Quanto mais longe, mais velocidade
-# 					#vel = Twist(Vector3(0.5,0,0), Vector3(0,0,0))##Codigo original do professor
-# ######################################################################################################					
-# 					if dif_x>20:
-# 						vel = Twist(Vector3(-1,0,0), Vector3(0,0,0))
-# 					elif dif_x>10:
-# 						vel = Twist(Vector3(-0.7,0,0), Vector3(0,0,0))
-# 					elif dif_x>5:
-# 						vel = Twist(Vector3(-0.5,0,0), Vector3(0,0,0))
-# 					elif dif_x>2:
-# 						vel = Twist(Vector3(-0.3,0,0), Vector3(0,0,0))
-# 					else:
-# 						vel = Twist(Vector3(-0.1,0,0), Vector3(0,0,0))
-# 				else:
-# 					if dif_x > 0: # Vira a direita
-# 						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
-# 					else: # Vira a esquerda
-# 						vel = Twist(Vector3(0,0,0), Vector3(0,0,0.2))
+
+			elif objeto_2:
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,0.56))#contorno_quadrado()
+
+			else:
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+
+
 			velocidade_saida.publish(vel)
 			print(vel)
 			rospy.sleep(0.01)
-######################################################################################################
 
 	except rospy.ROSInterruptException:
 	    print("Ocorreu uma exceção com o rospy")
--- scripts/findobjs.py
+++ scripts/findobjs.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 def findobj1(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(1,1),0)
