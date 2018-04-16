0a272ca8b41cc0b3685ddedaa05c3f38d83816e6
decoejz <decoejz@gmail.com>
2018-04-16 15:31:14 -0300

    add scan

---
+++ scripts/le_scan_sonny_18.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+
+import numpy as np
+
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+
+
+achou_perigo = False
+
+def scaneou(dado):
+	#print("LE SCAN")
+	global achou_perigo
+	achou_perigo = False
+	distancia_segura = 0.25
+
+	for i in range(0,46,1):
+		valor = dado.ranges[i]
+		if valor < dado.range_max and valor > dado.range_min:
+			if valor < distancia_segura:
+				achou_perigo = True
+					
+	for i in range(314,359,1):
+		valor = dado.ranges[i]
+		if valor < dado.range_max and valor > dado.range_min:
+			if valor < distancia_segura:
+				achou_perigo = True
